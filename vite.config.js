94f484760b79a558464a5b541fdeaa9c5795255f
dachis <dchimasanchez@ucdavis.edu>
2024-08-11 16:08:15 -0700

    Added build to deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://betochimas.github.io/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
